53afbfe1e2350713f2454a9f9f0edad5afc14037
satviktyagi <tyagi2020satvik@gmail.com>
2024-12-10 13:28:42 -0500

    removes extra files

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,6 +1,3 @@
 from datasets.aistdshadow import *
-from datasets.srdshadow import *
-from datasets.lrssshadow import *
-from datasets.selfshadow import *
 
-__all__ = ["AISTDShadow", "SRDShadow", "LRSSShadow", "SelfShadow"]
+__all__ = ["AISTDShadow"]
